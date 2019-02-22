98d3f2f40eb84603ead6514a51e25531d9f678f9
川石 <chuanshi.zl@alibaba-inc.com>
2019-02-22 15:13:07 +0800

    update make.sh

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
+# Tauris - 日志处理、清洗与转发工具
+
+## 背景
+
+
+
+## Tauris能做什么？
+* 它可以作为日志传输工具
+* 它可以作为消息适配器
+* 它可以作为阿里云日志服务和消息服务的客户端
+* 它可以分析日志得到监控数据
+
+
+
+Tauris在设计思想和配置方式方面借鉴了[logstash](https://www.elastic.co/cn/products/logstash)。
+
 # 目录
 1. [Introduction](docs/introduction.md)
 2. [Installation](docs/installation.md)
--- make.sh
+++ make.sh
@@ -4,4 +4,13 @@ export PATH=$JAVA_HOME/bin:$PATH
 base=`dirname $0`
 cd $base
 base=`pwd`
+version=`cat VERSION`
+rm -rf lib
 mvn package -Dmaven.test.skip=true -Ddist=${base}
+
+mkdir -p dist/tauris
+cp -R bin dist/tauris
+cp -R lib dist/tauris
+cp -R config dist/tauris
+tar cvf dist/tauris-${version}.tgz -C dist tauris
+rm -rf dist/tauris
